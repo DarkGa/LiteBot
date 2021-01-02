eec493b872a5294a33c9cc3e05ac00726823b371
DarkGa <48998251+DarkGa@users.noreply.github.com>
2021-01-02 02:10:10 +0200

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
 print('''
-LiteBot v1.0.8, Copyright (C) 2020-2020 DarkGamer <https://github.com/DarkGa>
+LiteBot v1.0.8, Copyright (C) 2020-2021 DarkGamer <https://github.com/DarkGa>
 Licensed under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE v3
 ''')
